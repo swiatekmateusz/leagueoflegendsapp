30c5baeb55ee8affa886bfacad381f79f0a3be88
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2019-09-11 15:02:50 +0200

    Updates

---
+++ precache-manifest.71917c79b4b246f9259e9cde94bca074.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc88627784ec0e98e7c8096ef283bd2",
+    "url": "/leagueoflegendsapp/index.html"
+  },
+  {
+    "revision": "a9665a1e2c211cbf21da",
+    "url": "/leagueoflegendsapp/static/css/main.1fb79ab9.chunk.css"
+  },
+  {
+    "revision": "10ad78dea0ae97f1714c",
+    "url": "/leagueoflegendsapp/static/js/2.6e282072.chunk.js"
+  },
+  {
+    "revision": "a9665a1e2c211cbf21da",
+    "url": "/leagueoflegendsapp/static/js/main.307f344d.chunk.js"
+  },
+  {
+    "revision": "ff0ab1285788f77c6958",
+    "url": "/leagueoflegendsapp/static/js/runtime~main.e672e6bd.js"
+  },
+  {
+    "revision": "34408a6b8e630358bcf64145a7108318",
+    "url": "/leagueoflegendsapp/static/media/friz-quadrata-std-bold-587034a220f9f.34408a6b.woff"
+  },
+  {
+    "revision": "60df3a3c690eb2c89801f68ebb088c4e",
+    "url": "/leagueoflegendsapp/static/media/friz-quadrata-std-bold-italic-587033d6d4298.60df3a3c.woff"
+  },
+  {
+    "revision": "9d3d183844306cf70d09b9f04a051808",
+    "url": "/leagueoflegendsapp/static/media/friz-quadrata-std-italic-587033b2c95df.9d3d1838.woff"
+  },
+  {
+    "revision": "8931e29d11c6879fa21b38e34427d39e",
+    "url": "/leagueoflegendsapp/static/media/friz-quadrata-std-medium-5870338ec7ef8.8931e29d.woff"
+  },
+  {
+    "revision": "315b3e4f61744de03beb683c73a5ccc3",
+    "url": "/leagueoflegendsapp/static/media/mainpagebg.315b3e4f.jpg"
+  }
+]);
